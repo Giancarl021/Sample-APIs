a3a8def853012eca8bebe7caf9ae37d9717a0973
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-02-27 23:07:20 -0300

    Initial files

---
+++ Images/server.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+const express = require('express');
+const cors = require('cors');
+const faker = require('@faker-js/faker').default;
+
+const app = express();
+const port = process.env.PORT || 80;
+
+app.use(cors());
+
+function getFn() {
+    const fns = Object.values(faker.image).filter(f => typeof f === 'function');
+    const randomFn = fns[Math.floor(Math.random() * fns.length)];
+
+    return randomFn;
+}
+
+app.get('*', (_, res) => {
+    const fn = getFn();
+    return res.json({
+        data: fn()
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
